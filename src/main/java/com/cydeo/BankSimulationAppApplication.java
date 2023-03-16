e1d75b673116a3324912ce4c174c55b449f5ebaf
kamilgurgener <kamilgurgener@gmail.com>
2023-03-15 20:48:18 -0400

    html pages were added th:if added with th:unless

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -20,35 +20,35 @@ public class BankSimulationAppApplication {
 
 		ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
 
-		AccountService accountService = container.getBean(AccountService.class);
-		TransactionService transactionService = container.getBean(TransactionService.class);
-
-		Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
-		Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.SAVING, 1L);
-		Account receiver2= null;
-
-		accountService.listAllAccount().forEach(System.out::println);
-
-		transactionService.makeTransfer(sender, receiver, new BigDecimal(35), new Date(), "Transaction 1");
-
-		transactionService.findAllTransaction().get(0);
-
-		accountService.listAllAccount().forEach(System.out::println);
-
-
-//		System.out.println("============ BalanceNotSufficientException ============");
-//		transactionService.makeTransfer(sender, receiver, new BigDecimal(100), new Date(), "Transaction 2");
+//		AccountService accountService = container.getBean(AccountService.class);
+//		TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//		Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
+//		Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.SAVING, 1L);
+//		Account receiver2= null;
+//
+//		accountService.listAllAccount().forEach(System.out::println);
+//
+//		transactionService.makeTransfer(sender, receiver, new BigDecimal(35), new Date(), "Transaction 1");
+//
+//		transactionService.findAllTransaction().get(0);
+//
+//		accountService.listAllAccount().forEach(System.out::println);
+//
+//
+////		System.out.println("============ BalanceNotSufficientException ============");
+////		transactionService.makeTransfer(sender, receiver, new BigDecimal(100), new Date(), "Transaction 2");
+////
+////		transactionService.findAllTransaction().get(1);
+////
+////		accountService.listAllAccount().forEach(System.out::println);
+//
+//		System.out.println("============ BadRequestException ============");
+//		transactionService.makeTransfer(sender, receiver2, new BigDecimal(100), new Date(), "Transaction 2");
 //
 //		transactionService.findAllTransaction().get(1);
 //
 //		accountService.listAllAccount().forEach(System.out::println);
-
-		System.out.println("============ BadRequestException ============");
-		transactionService.makeTransfer(sender, receiver2, new BigDecimal(100), new Date(), "Transaction 2");
-
-		transactionService.findAllTransaction().get(1);
-
-		accountService.listAllAccount().forEach(System.out::println);
 
 	}
 
